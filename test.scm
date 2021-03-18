8dc1020a5e627601d2a0c311a896ac4956cf6bc6
Shiro Kawai <shiro@acm.org>
2021-03-17 17:51:41 -1000

    Make test work

--- test.scm
+++ test.scm
@@ -8,15 +8,9 @@
 (use compat.sdff)
 (test-module 'compat.sdff)
 
-;; The following is a dummy test code.
-;; Replace it for your tests.
-(test* "test-gauche_compat_sdff" "gauche_compat_sdff is working"
-       (test-gauche_compat_sdff))
+(load "book/ch2.scm")
+
 
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
-
-
-
-
